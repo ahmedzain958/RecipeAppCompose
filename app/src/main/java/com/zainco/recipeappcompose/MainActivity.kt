55a5a4c6f5db2e2b162293753ab5bdda44cc349f
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2022-11-21 19:34:53 +0200

    feat: setup compose configurations

--- app/src/main/java/com/zainco/recipeappcompose/MainActivity.kt
+++ app/src/main/java/com/zainco/recipeappcompose/MainActivity.kt
@@ -2,10 +2,22 @@ package com.zainco.recipeappcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column {
+                modifier = Modifier.padding(16.dp)
+            }
+            Text(text = "Zainco")
+        }
+
     }
 }
